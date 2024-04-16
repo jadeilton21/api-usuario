6b0769fadaf035673eac67d382b487f58e563aee
Jadeilton <jadeiltondm@gmail.com>
2024-04-16 20:18:12 -0300

    feat: restController and RequestMapping

--- src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
+++ src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
@@ -1,4 +1,9 @@
 package api.usuario.segunda.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pessoa")
 public class PessoaControllers {
 }
