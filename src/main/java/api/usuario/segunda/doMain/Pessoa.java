653d7f426aff0d010d0793375ad0e2b94387f4e8
Jadeilton <jadeiltondm@gmail.com>
2024-04-16 16:20:09 -0300

    feat: create constructor

--- src/main/java/api/usuario/segunda/doMain/Pessoa.java
+++ src/main/java/api/usuario/segunda/doMain/Pessoa.java
@@ -5,4 +5,10 @@ public class Pessoa {
 
     private String login;
     private String senha;
+
+
+    public Pessoa(String login,String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
 }
