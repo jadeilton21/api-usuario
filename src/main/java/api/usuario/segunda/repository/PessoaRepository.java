6fba176e9049ea03c3eefe88f7e6f0005f2cd25a
Jadeilton <jadeiltondm@gmail.com>
2024-04-17 07:56:38 -0300

    feat: add anotation Repository

--- src/main/java/api/usuario/segunda/repository/PessoaRepository.java
+++ src/main/java/api/usuario/segunda/repository/PessoaRepository.java
@@ -1,4 +1,8 @@
 package api.usuario.segunda.repository;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Repository
 public class PessoaRepository {
 }
