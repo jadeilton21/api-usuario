2610a4480ae74d6cf2da094d294782f03d79f7e4
Jadeilton <jadeiltondm@gmail.com>
2024-04-17 07:52:22 -0300

    feat: PessoaRepository add

--- src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
+++ src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
@@ -1,5 +1,6 @@
 package api.usuario.segunda.controllers;
 
+import api.usuario.segunda.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
