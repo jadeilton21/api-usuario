8fef23a79b5867cc6066e65a382216c732590fb8
Jadeilton <jadeiltondm@gmail.com>
2024-04-17 08:03:06 -0300

    feat: create method cadastrarPessoa

--- src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
+++ src/main/java/api/usuario/segunda/controllers/PessoaControllers.java
@@ -1,8 +1,11 @@
 package api.usuario.segunda.controllers;
 
+import api.usuario.segunda.doMain.Pessoa;
 import api.usuario.segunda.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,10 @@ public class PessoaControllers {
 
     @Autowired
     private PessoaRepository repository;
+
+
+    public void cadastrarPessoa(@RequestBody Pessoa pessoa){
+
+
+    }
 }
