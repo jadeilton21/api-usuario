12d3951c507d9ecfe06f7d0aca3b337c1c773e2d
Jadeilton <jadeiltondm@gmail.com>
2024-04-17 08:10:45 -0300

    feat: create method save

--- src/main/java/api/usuario/segunda/repository/PessoaRepository.java
+++ src/main/java/api/usuario/segunda/repository/PessoaRepository.java
@@ -1,8 +1,13 @@
 package api.usuario.segunda.repository;
 
+import api.usuario.segunda.doMain.Pessoa;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Repository
 public class PessoaRepository {
+
+    public void save(Pessoa pessoa) {
+
+    }
 }
